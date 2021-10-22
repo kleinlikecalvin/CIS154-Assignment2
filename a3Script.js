4cff864c00a9bf8007d733f72e079d1f23207187
streetlightkids <ltj_hq@hotmail.com>
2021-10-22 11:06:35 -0700

    This is a mess
    - need to get the image, description and url to put in the href around the image
    -look at notes fro Nate in Slack
    -

--- a3Script.js
+++ a3Script.js
@@ -1,15 +1,20 @@
 $(loadXml);
 function loadXml(){
-    $.get("https://cors-anywhere.herokuapp.com/https://feeds.simplecast.com/54nAGcIl").done(function(feed){
+    $.get("https://cors-anywhere.herokuapp.com/https://rss.art19.com/apology-line").done(function(feed){
         const story = $(feed).find("item");
-        const randomNum = Math.floor(Math.random() * 100);
+        const randomNum = Math.floor(Math.random() * 10);
+        console.log(randomNum)
         $("#genBtn").click(function(){
-            if(randomNum <= story.length){
-                $("#image").html();//image
-                $("#webLink").html();//href
-                $("#title").html(story.find("title")[randomNum].textContent);//title
-                $("#description").html(story.find("description")[randomNum].textContent);//description
-            }
+
+                // $("#image").html(story.find("itunes:image")[randomNum].attr("href"));//image
+                // let linkSpot = $("#webLink").attr("href") 
+                let link = story.find("guid")[randomNum];//href
+                console.log(link);
+                // linkSpot.text(link);
+            let title = story.find("title")[randomNum];
+            $("#title").html(title);//title
+                // $("#description").html(story.find("description")[randomNum].text);//description
+            
         });//end click() and nested anon functions
     }).fail(function(){
         console.log("Error message: ", status);
@@ -21,7 +26,6 @@ function loadXml(){
 /* 
 WHERE I LEFT OFF 10/15/2021
 -HAVE TO sign "login" to the cors-anywhere site first
--Last thing I did was I changed all the append() to html()
 -Need to figure out the page link url, how to get to the images, and inject new elements by using the html() rather than just injecting content into the already created elements
 -Write if/else statement for images if they're undefined show default photo <--- if
 -Maybe find a new RSS feed
