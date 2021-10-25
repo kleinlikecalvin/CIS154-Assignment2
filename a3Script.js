ff271087ae13eb2ebab0d3f206ccb73db95c0582
streetlightkids <ltj_hq@hotmail.com>
2021-10-24 19:16:05 -0700

    calling it a day
    -description doesn't work
    -link doesn't work
    -image doesn't work

--- a3Script.js
+++ a3Script.js
@@ -3,20 +3,29 @@ function loadXml(){
     $.get("https://cors-anywhere.herokuapp.com/https://podcastfeeds.nbcnews.com/HL4TzgYC").done(function(feed){
         $("#genBtn").click(function(){
             const randomNum = Math.floor(Math.random() * 500);
+
             const story = $(feed).find("item")[0];
             console.log(story);
-                let image = $(story).find("itunes:image");//image 
-                console.log(image);
-                // let imageUrl = 
-                 
-                // $("#image").html(image);     
-                console.log(image);      
-                let link = story.find("link")[randomNum].textContent;//href
-                $("#webLink").attr("href", link);
-                let title = story.find("title")[randomNum];
-                $("#title").html(title);//title
-                let description = story.find("description")[randomNum].textContent;//description
-                $("#description").html(description + "<br><br> Click the image for more details!");
+
+            let title = $(story).find("title");
+            console.log(title);
+            $("#title").html(title);//title
+
+            let description = $(story).find("itunes\\:summary");//description
+            console.log(description);
+            $("#description").html(description + "<br><br> Click the image for more details!");
+
+            let link = $(story).find("link");//href
+            console.log(link);
+            $("#webLink").attr("href", link);
+            
+            // let image = $(story).find("image");//image 
+            // console.log(image);
+            // let imageUrl = $(image).attr("url");
+            // console.log(imageUrl);
+            // $("#image").html(image);   
+
+
         });//end click() and nested anon functions
     }).fail(function(){
         console.log("Error message: Something isn't right...");
